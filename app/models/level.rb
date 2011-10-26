27fc125ab8c0fb2e8bc7a80a4be2c33499604363
James Dunwoody <jamesdunwoody@gmail.com>
2011-10-26 23:01:57 +1100

    Modified header. Created levels dropdown. Added .rvmrc to rvm autoload

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,3 +1,4 @@
 class Level < ActiveRecord::Base
   has_many :category
+  belongs_to :progression
 end
--- db/migrate/20111017114131_create_levels.rb
+++ db/migrate/20111017114131_create_levels.rb
@@ -2,7 +2,7 @@ class CreateLevels < ActiveRecord::Migration
   def change
     create_table :levels do |t|
       t.string :name
-      t.references :level
+      t.references :progression
       t.references :category
 
       t.timestamps
